f44286ca1f0feb8c16cf56bc2c296ec52cbbc23e
Brandon Leventhal <bleventh@gmail.com>
2013-09-22 21:18:16 -0700

    Not needed

--- passwordMaker.js
+++
@@ -1,9 +0,0 @@
-var bcrypt = require('bcrypt-nodejs');
-var pass;
-bcrypt.hash("cookies", null, null, function(err, hash) {
-   console.log(hash);
-   bcrypt.compare("cookies", hash, function(err, res) {
-      console.log(res == true);
-});
-});
-
